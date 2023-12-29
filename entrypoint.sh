8ce9acad261b031bede89c72eb2121cfcde5adb5
Yoni FIRROLONI <pro@yoni-firroloni.com>
2023-12-29 02:10:48 +0100

    fix: conf file

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 cat <<EOF >ServerConfig.toml
 [General]
-Description = ${Description}
+Description = "${Description}"
 Debug = ${Debug} # true or false to enable debug console output
 Private = ${Private} # Private?
 Port = ${Port} # Port to run the server on UDP and TCP
@@ -10,7 +10,6 @@ MaxCars = ${MaxCars} # Max cars for every player
 MaxPlayers = ${MaxPlayers} # Maximum Amount of Clients
 Map = "${Map}" # Default Map
 Name = "${Name}" # Server Name
-Desc = "${Desc}" # Server Description
 use = "${use}" # Resource file name
 AuthKey = "${AuthKey}" # Auth Key
 EOF
